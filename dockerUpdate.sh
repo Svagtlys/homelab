0779c7c82f77eeb62bd37e7f0d228a7938e61bfe
Morgan Wyatt <45446348+Svagtlys@users.noreply.github.com>
2024-05-17 14:05:16 +0000

    Custom Builds Update Modifed dockerUpdate.sh to run custom builds Added custom build for tandoor recipes app to allow full usability of container run by non-root

--- dockerUpdate.sh
+++ dockerUpdate.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Custom builds
+(cd knowledgeBase/;     docker build -t tandoor:latest - < tandoorDockerfile)
+docker build -t qmcgaw/ddns-updater https://github.com/qdm12/ddns-updater.git --build-arg UID=1006 --build-arg GID=1006
+(cd mediaManagement/hyperion-ng/; docker build -t hyperion-ng .)
+
+# Normal updates and restart
+
 (cd homeAutomation/;    docker compose pull --ignore-pull-failures; docker compose up -d)
 (cd knowledgeBase/;     docker compose pull --ignore-pull-failures; docker compose up -d)
 (cd mediaManagement/;   docker compose pull --ignore-pull-failures; docker compose up -d)
